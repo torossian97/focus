65968aeb8ec2054f92e71f158cd5b9f93d0c7052
Antoine Torossian <torossian97@gmail.com>
2023-02-23 14:10:11 -0800

    project inception

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import Slider from '@mui/material/Slider';
 
 function App() {
+
+  const [value, setValue] = React.useState(
+    30,
+  );
+
+  const handleSliderChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleInputChange = (event) => {
+    setValue(event.target.value === '' ? '' : Number(event.target.value));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Slider
+          aria-label="Temperature"
+          defaultValue={30}
+          onChange={handleSliderChange}
+          color="secondary"
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
