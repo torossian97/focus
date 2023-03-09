9c6b0c277c92a79cd7a21cafa0fa1e6694a4b135
“Nick <nickvolkov@outlook.com>
2023-03-08 21:00:00 -0800

    landing page, header, footer all done

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './footer.css';
-import logoUrl from '../../assets/forcemeto.svg';
+import logoUrl from '../../assets/logoBlack.png';
 import {Link} from "react-router-dom";
 
 const Footer = () => (
   <div className="footer">
       <Link to="/"><img className="logoFooter" src={logoUrl} alt="ForceMeTo"/></Link>
-      <h6>© 2023 Vosian Technologies, Inc.</h6>
+      <h6>© 2023 Volian Technologies, Inc.</h6>
   </div>
 )
 export default Footer;
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import './header.css';
-import logoUrl from '../../assets/forcemeto.svg';
+import logoUrl from '../../assets/logoRed.png';
 import {Link} from "react-router-dom";
 
 window.onscroll = function() {myFunction()};
+window.onload = function(){ 
+  var burger = document.querySelector('.burgerContainer');
+  var headerr = document.querySelector('.mobileHeaderContainer');
+  burger.onclick = function() {myFunction2()};
+  function myFunction2() {
+    headerr.classList.toggle('menu-opened');
+  }
+}
 
 function myFunction() {
   if (document.documentElement.scrollTop > 50 || document.documentElement.scrollTop > 50) {
@@ -14,30 +22,49 @@ function myFunction() {
 }
 
 const Header = () => (
+
   <div className="header">
     <div className="headerContents">
-    <Link to="/"><img className="logo" src={logoUrl} alt="ForceMeTo"/></Link>
-    <div className='headerNavigation'>
-      <div className='headerNavigationLeft'> 
+      <Link to="/"><img className="logo" src={logoUrl} alt="ForceMeTo"/></Link>
+      <div className='headerNavigation'>
+        <div className='headerNavigationLeft'> 
+          </div>
+          <div className='headerNavigationRight'>
+          <div className="navigationObject">
+            <Link to="/">Home</Link>
+          </div>
+          <div className="navigationObject">
+            <Link to="/">How it works</Link>
+          </div>
+          <div className='headerDivider'></div>
+          <div className="navigationObject">
+            <Link to="/demo">Contact support</Link>
+          </div>
+          
+          <div className='navigationButton'>Force me to [write]</div>
         </div>
-        <div className='headerNavigationRight'>
-        <div className="navigationObject">
-          <Link to="/">Home</Link>
-        </div>
-        <div className="navigationObject">
-          <Link to="/">How it works</Link>
-        </div>
-        <div className='headerDivider'></div>
-        <div className="navigationObject">
-          <Link to="/demo">Contact support</Link>
-        </div>
-        
-        <div className='navigationButton'>Force me to [write]</div>
       </div>
     </div>
-    
+    <div id="divider"></div>
+
+    <div class="mobileHeader">
+      <div class="mobileHeaderContainer">
+        <Link to="/"><img className="mobileLogo" src={logoUrl} alt="ForceMeTo"/></Link>
+          <div class="burgerContainer">
+              <div id="burger">
+                  <div class="bar topBar"></div>
+                  <div class="bar btmBar"></div>
+              </div>
+          </div>
+          <ul class="menu">
+              <li class="menu-item"><a href="#">Home</a></li>
+              <li class="menu-item"><a href="#">How it works</a></li>
+              <li class="menu-item"><a href="#">Contact support</a></li>
+              <li class="menu-item"><div className='navigationButton'>Force me to [write]</div></li>
+          </ul>
+      </div>
+    </div>
   </div>
-  <div id="divider"></div>
-  </div>
+
 )
 export default Header;
